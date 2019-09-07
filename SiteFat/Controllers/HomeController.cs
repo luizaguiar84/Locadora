d4bd432b74fa497c53b0974be0bb76efdb8fec26
JJPontes <50496581+JJPontes@users.noreply.github.com>
2019-09-07 11:09:54 -0300

    Testando o pull
    Para funcionar o bd precisa trocar a ConnectionString

---
+++ SiteFat/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteFat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Alugar()
+        {
+            return View();
+        }
+
+        public ActionResult Servicos()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ SiteFat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SiteFat.MvcApplication" Language="C#" %>
