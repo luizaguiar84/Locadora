af642ae881f6d038f9293712f3cde7575ef6aa47
luizaguiar <luizmanoel00@gmail.com>
2019-09-14 03:39:41 -0300

    Atualizacao do projeto

--- SiteFat/Controllers/HomeController.cs
+++ SiteFat/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dll_BS_Fat;
+using Dll_DB_Fat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,21 +10,27 @@ namespace SiteFat.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
+		List<Veiculos> model;
 
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult Alugar()
-        {
-            return View();
-        }
+		public ActionResult Alugar()
+		{
+			model = new List<Veiculos>();
+			var veiculos = new VeiculosDao().GetAll();
+			foreach (var veiculo in veiculos)
+			{
+				model.Add(veiculo);
+			}
+			return View(model);
+		}
 
-        public ActionResult Servicos()
-        {
-
-            return View();
-        }
-    }
+		public ActionResult Servicos()
+		{
+			return View();
+		}
+	}
 }
