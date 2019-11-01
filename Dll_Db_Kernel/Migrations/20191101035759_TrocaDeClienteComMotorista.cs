66c46cff28b1325ed55e6e26a1b118250004104a
Luiz Aguiar <luizmanoel00@gmail.com>
2019-11-01 00:59:14 -0300

    migration tabela motoristas

---
+++ Dll_Db_Kernel/Migrations/20191101035759_TrocaDeClienteComMotorista.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbKernel.Migrations
+{
+    public partial class TrocaDeClienteComMotorista : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Profissao",
+                table: "ClientesPF");
+
+            migrationBuilder.DropColumn(
+                name: "TipoCliente",
+                table: "ClientesPF");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Renavam",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Veiculos",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Montadora",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataAquisicao",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cor",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Combustivel",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Chassi",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnoModelo",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Usuarios",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "TipoDespesa",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Marca",
+                table: "Pneus",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Pecas",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Obrigacoes",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Peca",
+                table: "Estoque",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Enderecos",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 9,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "ControlePatio",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Validade",
+                table: "Cnhs",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Cnhs",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Emissao",
+                table: "Cnhs",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "ClientesPF",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Nascimento",
+                table: "ClientesPF",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "ClientesPF",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Combustivel",
+                table: "Abastecimentos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Renavam",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 7);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Montadora",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataAquisicao",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cor",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Combustivel",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Chassi",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnoModelo",
+                table: "Veiculos",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Usuarios",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "TipoDespesa",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Marca",
+                table: "Pneus",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Pecas",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Obrigacoes",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Peca",
+                table: "Estoque",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Enderecos",
+                maxLength: 9,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "ControlePatio",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Validade",
+                table: "Cnhs",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Cnhs",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Emissao",
+                table: "Cnhs",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "ClientesPF",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Nascimento",
+                table: "ClientesPF",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "ClientesPF",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Profissao",
+                table: "ClientesPF",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipoCliente",
+                table: "ClientesPF",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Combustivel",
+                table: "Abastecimentos",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
